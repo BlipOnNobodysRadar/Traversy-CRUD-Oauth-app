73e67a0b94e9fba0a9300c28e8a92e5dfa13dd97
Ryan Lowe <somebody480@gmail.com>
2022-08-07 19:31:43 -0400

    partial progress update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
@@ -22,8 +23,11 @@ if (process.env.NODE_ENV === "development") {
 
 //Handlebars
 //!Add the word .engine after exphbs
-app.engine(".hbs", exphbs.engine({ extname: ".hbs" }));
+app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.use("/", require("./routes/index"));
